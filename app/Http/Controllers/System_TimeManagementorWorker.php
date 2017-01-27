5465de41a3c2904acdfc9526f5ab3262268d249c
Rana <rana@Ranas-MacBook-Pro.local>
2017-01-27 14:46:16 +0700

    Uta

---
+++ app/Http/Controllers/System_TimeManagementorWorker.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use DateTime;
+use Date;
+use Validator;
+use Input;
+use Response;
+class System_TimeManagementorWorker extends Controller
+{
+     
+}
