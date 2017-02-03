c401e3cf3feebace9992bb7c16ec8a788acdaf84
Rana <rana@Ranas-MacBook-Pro.local>
2017-02-03 11:56:10 +0700

    time

--- app/Http/Controllers/System_TimeManagementorWorker.php
+++ app/Http/Controllers/System_TimeManagementorWorker.php
@@ -25,9 +25,14 @@ class System_TimeManagementorWorker extends Controller
              ->get();
       $process = DB::table('process_master')->get();
       $default = $locations[2]->location_id;
+      //generate JP timezone
+      $tz = 'Asia/Tokyo';
+      $timestamp = time();
+      $dt = new DateTime("now", new DateTimeZone($tz)); //first argument "must" be a string
+      $dt->setTimestamp($timestamp); //adjust the object to correct timestamp
+      $time =  $dt->format('d.m.Y, H:i:s');
 
-
-     return view ('TimeManagementIndividual',compact('default','staff','locations','process'));
+     return view ('TimeManagementIndividual',compact('default','staff','locations','process','time'));
   }
   catch(\Exception $e)
       {
